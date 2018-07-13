e83fc477b342eaacfea9ffc96235e6e485631010
erinawidjaja <erina.widjaja@gmail.com>
2018-07-13 21:44:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
 # dog.rb
+class Dog
+  attr_accessor :bark, :sit 
+  def initialize(bark, sit)
+    @bark = bark
+    @sit = sit 
+  end 
+end 
+dog_one = Dog.new("Woof!", "The Dog is sitting")
+puts dog_one.bark
+puts dog_one.sit 
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
 # person.rb
+class Person 
+  attr_accessor :talk 
+  def initialize(talk)
+    @talk = talk
+  end 
+end 
+person_one = person.new("Hello World!")
+puts person_one.talk 
+
+    
